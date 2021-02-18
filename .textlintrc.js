91b1221ccc8b066883a9c97633f42cce3e29c555
philomagi <tooppoo56@gmail.com>
2021-02-19 02:59:02 +0900

    add textlint

---
+++ .textlintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  filters: {},
+  rules: {
+    "ja-no-abusage": true,
+    "ja-no-mixed-period": true,
+    "no-double-negative-ja": true,
+    "no-doubled-conjunction": true,
+    "no-doubled-joshi": true,
+    "no-mix-dearu-desumasu": true,
+    "web-plus-db": true,
+  }
+}
