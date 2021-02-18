238243ab5d88cd4ae2b137c8c27151f496080105
philomagi <tooppoo56@gmail.com>
2021-02-19 03:37:25 +0900

    fix lint

--- .textlintrc.js
+++ .textlintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
-  filters: {},
+  filters: {
+    allowlist: {
+      allow: [
+        "/^\\[\\^[0-9]+\\]:.+$/", // 引用
+      ],
+    },
+  },
   rules: {
     "ja-no-abusage": true,
     "ja-no-mixed-period": true,
@@ -7,6 +13,7 @@ module.exports = {
     "no-doubled-conjunction": true,
     "no-doubled-joshi": true,
     "no-mix-dearu-desumasu": true,
+    "period-in-list-item": true,
     "web-plus-db": true,
   }
 }
