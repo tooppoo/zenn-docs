3a2d0b986a644599d8acf1120e9fc1583f05c32d
philomagi <tooppoo56@gmail.com>
2021-09-13 00:46:29 +0900

    fix error handling

--- scripts/books/book-to-pdf/src/cli/index.js
+++ scripts/books/book-to-pdf/src/cli/index.js
@@ -3,6 +3,8 @@ const Cli = require('./cli-conversion-process')
 
 const arg = new Cli.CliArgument(process.argv, process.cwd())
 
-Cli.processConversion(arg).finally(() => {
-  process.exit()
+Cli.processConversion(arg).then(() => {
+  process.exit(0)
+}).catch(() => {
+  process.exit(1)
 })
