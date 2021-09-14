b2c5dd511b3ee0347ca0607ec91414c9fc4655a4
philomagi <tooppoo56@gmail.com>
2021-09-15 01:58:53 +0900

    use serialized book

--- scripts/books/book-to-pdf/src/core/conversion/converter/pdf-converter.js
+++ scripts/books/book-to-pdf/src/core/conversion/converter/pdf-converter.js
@@ -1,8 +1,6 @@
-const { exec } = require('child_process')
 const { default: mdToPdf } = require('md-to-pdf')
-const path = require('path')
 const { logger } = require('../../../util/logger')
-const { Book } = require('../../book/book')
+const { SerializedBook } = require('../../serializer/serialized-book')
 
 /**
  * @augments Converter
@@ -10,68 +8,16 @@ const { Book } = require('../../book/book')
 class PdfConverter {
   /**
    * @inheritdoc
-   * @param {Book} book
+   * @param {SerializedBook} book
    * @param {string} outPath
    * @returns {Promise<void>}
    */
   async convert(book, outPath) {
-    const sectionPaths = book.orderedSections.map(
-      sec => path.resolve(book.path, `${sec.title}.md`)
-    )
-    const commandToConcatenate = `cat ${sectionPaths.join(' ')}`
-    logger.debug({ book, outPath, command: commandToConcatenate })
-
-    const concatenatedContent = await this.concatContent(commandToConcatenate)
-    // remove metadata
-    const content = concatenatedContent
-      .replace(/-+\n/g, '\n')
-      .replace(/title:\s+(.+)\n/g, `# $1\n`)
-      .replace(/[a-zA-Z]+: .+\n/g, '')
-
-    await this.markdownToPdf(content, outPath)
-  }
-
-  /**
-   * @private
-   * @param {string} command 
-   * @returns {Promise<string>} 
-   */
-  concatContent(command) {
-    logger.info('start concatenate markdown documents')
-
-    return new Promise((resolve, reject) => {
-      try {
-        exec(command, (error, stdout, stderr) => {
-          if (error) {
-            logger.error({ stderr }, 'failed concatenate markdown documents')
-
-            reject(error)
-          } else {
-            logger.info('succeed concatenate markdown documents')
-
-            resolve(stdout)
-          }
-        })
-      } catch (error) {
-        logger.error({ error }, 'unknown error in concatenating documents')
-
-        reject(error)
-      }
-    })
-  }
-
-  /**
-   * @private
-   * @param {string} content 
-   * @param {string} outPath
-   * @returns {Promise<void>}
-   */
-  async markdownToPdf(content, outPath) {
     logger.info('start pdf conversion')
-    logger.debug({ content, outPath })
+    logger.debug({ book, outPath })
 
     try {
-      await mdToPdf({ content }, { dest: outPath })
+      await mdToPdf({ content: book.content }, { dest: outPath })
 
       logger.info('succeed pdf conversion')
     } catch (error) {
